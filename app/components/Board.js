4fef0818d5c9fa524cdb68b00d9d40c0226bf0d1
Jira Vinyoopongphan <jira.vinyoo@gmail.com>
2016-04-04 21:32:18 -0700

    (feat) Connected components to application state

--- app/components/Board.js
+++ app/components/Board.js
@@ -1,34 +1,48 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import ReactGridLayout from 'react-grid-layout'
 import Cell from './Cell'
 import '../styles/board.css';
 import '../styles/react-grid-layout.css';
 
-const makeBoard = ['X', 'X', 'O', 'X', 'O', 'X', 'X', 'O', 'X']
+//TODO: pull from state
+//const makeBoard = [
+//  {x: 0, y: 0, w: 4, h: 4, static: true, value: 'x'},
+//  {x: 4, y: 0, w: 4, h: 4, static: true, value: ''},
+//  {x: 8, y: 0, w: 4, h: 4, static: true, value: ''},
+//  {x: 0, y: 4, w: 4, h: 4, static: true, value: 'x'},
+//  {x: 4, y: 4, w: 4, h: 4, static: true, value: 'x'},
+//  {x: 8, y: 4, w: 4, h: 4, static: true, value: ''},
+//  {x: 0, y: 8, w: 4, h: 4, static: true, value: ''},
+//  {x: 4, y: 8, w: 4, h: 4, static: true, value: 'o'},
+//  {x: 8, y: 8, w: 4, h: 4, static: true, value: 'x'}
+//]
+
 class Board extends Component {
-  clickHandler(a) {
-    console.log('this is ', a)
+  constructor(props) {
+    super(props)
   }
+
   render() {
-    const cells = makeBoard.map((cellValue, index) => {
-      return (<Cell key={index} marker={cellValue}/>)
+    const cells = this.props.board.map((cellValue, index) => {
+      return (
+        <div className="layout line" key={index} _grid={cellValue}>
+          <Cell position={{x: cellValue.x, y: cellValue.y, value: cellValue.value}}/>
+        </div>
+      )
     })
     return (
-
         <ReactGridLayout className="layout" cols={15} rowHeight={20} width={500}>
-          <div onClick={this.clickHandler.bind()} className="line" key="a" _grid={{x: 0, y: 0, w: 4, h: 4, static: true}}>a</div>
-          <div className="line" key="b" _grid={{x: 4, y: 0, w: 4, h: 4, static: true}}>b</div>
-          <div className="line" key="c" _grid={{x: 8, y: 0, w: 4, h: 4, static: true}}>c</div>
-          <div className="line" key="d" _grid={{x: 0, y: 4, w: 4, h: 4, static: true}}>d</div>
-          <div className="line" key="e" _grid={{x: 4, y: 4, w: 4, h: 4, static: true}}>e</div>
-          <div className="line" key="f" _grid={{x: 8, y: 4, w: 4, h: 4, static: true}}>f</div>
-          <div className="line" key="g" _grid={{x: 0, y: 8, w: 4, h: 4, static: true}}>g</div>
-          <div className="line" key="h" _grid={{x: 4, y: 8, w: 4, h: 4, static: true}}>h</div>
-          <div className="line" key="i" _grid={{x: 8, y: 8, w: 4, h: 4, static: true}}>i</div>
+          {cells}
         </ReactGridLayout>
-
     )
   }
 }
 
-export default Board
+const mapStateToProps = (state) => {
+  return {
+    board: state.game.board
+  }
+}
+
+export default connect(mapStateToProps, null)(Board)
--- app/components/Cell.js
+++ app/components/Cell.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { makeMove } from '../actions/index'
+import ReactGridLayout from 'react-grid-layout'
 import '../styles/board.css';
 
 class Cell extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  handleClick(a) {
+    //TODO: dispatch action to mark board
+    console.log('this state', this.props.playerTurn)
+    console.log('this is ', a)
+    this.props.makeMove({x: a.x, y: a.y})
+  }
+
   render() {
-    console.log('props', this.props)
-    const { marker } = this.props
-    console.log('marker', marker)
     return (
-      <div className="cell">{marker}</div>
+      <div className="cell" onClick={() => {this.handleClick(this.props.position)}}>
+        {/*<img src={`${this.state.marker}`} alt=""/>*/}
+        {this.props.position.value}
+      </div>
     )
   }
 }
 
-export default Cell
+const mapStateToProps = (state) => {
+  return {
+    playerTurn: state.game.playerTurn
+  }
+}
+
+export default connect(mapStateToProps, { makeMove })(Cell)
